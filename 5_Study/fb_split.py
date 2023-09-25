283cc3d63870012d48a5639ced824d0e09320a72
Youm <111734605+meaningful96@users.noreply.github.com>
2023-09-25 12:42:01 +0900

    Add files via upload

--- 5_Study/fb_split.py
+++ 5_Study/fb_split.py
@@ -1,3 +1,9 @@
+"""
+Created on meaningful96
+
+DL Project
+"""
+
 import json
 from typing import List, Dict, Tuple
 from collections import defaultdict, deque
@@ -14,7 +20,13 @@ class LinkGraph:
         
         for ex in examples:
             head_id, head, relation, tail_id, tail = ex['head_id'], ex['head'], ex['relation'], ex['tail_id'], ex['tail']
+        
+            # Add to graph with all details
             self.graph[head_id].add((head_id, head, relation, tail_id, tail))
+        
+            # Check if tail_id is not in the graph and then add the corresponding head details
+            self.graph[tail_id].add((tail_id, tail, relation, head_id, head))
+
         print('Done building link graph with {} nodes'.format(len(self.graph)))
 
     def get_neighbor_ids(self, tail_id: str, n_hops: int) -> List[str]:
@@ -50,26 +62,24 @@ class LinkGraph:
         
     def create_positive_samples(self, head_id: str, tail_id: str, tail_hops: int, max_samples: int) -> List[Dict[str, any]]:
         link_graph = self.graph
-    
+        
         # Exclude tail entity and its neighbors determined by tail_hops.
         exclude_ids = self.get_neighbor_ids(tail_id, tail_hops)
         exclude_ids.append(tail_id)
         exclude_ids.append(head_id)
     
         # Calculate the hop distance for all entities starting from the head entity.
-        hop_distances = self.bfs(head_id)
-    
-        """
+        hop_distances = self.bfs(head_id)    
+        
         # On average, for WN18RR, hard negative samples with a batch size of 256 fall within a 6-hop boundary. 
         # Similarly, samples from FB15K-237 with a batch size of 1024 encompass nearly all triples within 5 hops.
-        """
     
         # Sort entities based on their proximity to the head entity.
         sorted_entities = sorted(hop_distances.keys(), key=lambda k: hop_distances[k])
 
         results = []
         sample_count = 0
-    
+        
         for entity in sorted_entities:
             # Stop if we've collected the desired number of samples.
             if sample_count >= max_samples:
@@ -78,54 +88,122 @@ class LinkGraph:
             # Retrieve triples that are associated with the current entity.
             for triple in link_graph.get(entity, []):
                 # Add the triple to the sample list if the tail is not in the exclude_ids.
-                if triple[0] and triple[3] not in exclude_ids:
+                if triple[0] not in exclude_ids and triple[3] not in exclude_ids:
                     results.append({
                         "head_id": triple[0],
                         "head": triple[1],
                         "relation": triple[2],
                         "tail_id": triple[3],
                         "tail": triple[4]
-                        })
+                    })
                     sample_count += 1
                 
                     # Stop if we've collected the desired number of samples.
                     if sample_count >= max_samples:
                         break
+        
+        # # Logic to add random samples for disconnected entities
+        # all_entities = list(self.graph.keys())
+        # unvisited_entities = set(all_entities) - set(hop_distances.keys())
+        
+        # while sample_count < max_samples and unvisited_entities:
+        #     random_entity = random.choice(list(unvisited_entities))
+        #     random_sample = random.choice(list(link_graph[random_entity]))
+        #     if random_sample[0] not in exclude_ids and random_sample[3] not in exclude_ids:
+        #         results.append({
+        #             "head_id": random_sample[0],
+        #             "head": random_sample[1],
+        #             "relation": random_sample[2],
+        #             "tail_id": random_sample[3],
+        #             "tail": random_sample[4]
+        #         })
+        #         sample_count += 1
 
-        return results 
-        
-        
+        #     unvisited_entities.remove(random_entity)
+
+        return results
+
+# """
 start_time = time.time()    
 train_path_fb = "C:/Users/USER/Desktop/json/FB15k237/train.txt.json"
-G_fb = LinkGraph(train_path_fb)
-
 with open(train_path_fb, 'r', encoding='utf-8') as f:
-    train_data_wn = json.load(f)
+    train_data_fb = json.load(f)
 
-# A list to store the results
+G_wn = LinkGraph(train_path_fb)
+
 all_positive_samples = []
-
 # Iterate over the WordNet train set
-for example in train_data_wn:
+for example in train_data_fb:
     head_id = example['head_id']
     tail_id = example['tail_id']
     
     # Create positive samples
-    positive_samples = G_fb.create_positive_samples(head_id, tail_id, 1, 1023)
-    positive_samples.insert(0, example)
+    positive_samples = G_wn.create_positive_samples(head_id, tail_id, 1, 100)
+    positive_samples.insert(0,example) 
     # Add the results to the list
     all_positive_samples.append(positive_samples)
 
 # Save the combined results to a JSON file
-output_path = "/home/youminkk/Paper_reconstruction/SimKGC_HardNegative/data/FB15k237/positive_samples_valid.json"
+output_path = "C:/Users/USER/Desktop/json/FB15k237/test_HN.json"
 with open(output_path, 'w', encoding='utf-8') as f:
     json.dump(all_positive_samples, f, ensure_ascii=False, indent=4)
 
 end_time = time.time()
 sec = end_time - start_time
 
-print("Total Length",len(all_positive_samples))
-print("Positive samples of FB15K-237 saved")
-print("Taking time:", datetime.timedelta(seconds = sec))
+print("Total Length:", len(all_positive_samples))
+print("Positive samples of FB15k-237 saved")
+print("Taking time:",datetime.timedelta(seconds = sec))
+
 print("Done!!")
+# """
+"""
+start_time = time.time()    
+train_path_wn = "C:/Users/USER/Desktop/json/FB15k237/train.txt.json"
+with open(train_path_wn, 'r', encoding='utf-8') as f:
+    train_data_wn = json.load(f)
+
+G_wn = LinkGraph(train_path_wn)
+
+valid_path_wn = "C:/Users/USER/Desktop/json/FB15k237/valid.txt.json"
+with open(valid_path_wn, 'r', encoding = 'utf-8') as f:
+    valid_data_wn = json.load(f)
+
+all_positive_samples = []
+# Iterate over the WordNet train set
+for example in valid_data_wn:
+    head_id = example['head_id']
+    tail_id = example['tail_id']
+    
+    # Create positive samples
+    positive_samples = G_wn.create_positive_samples(head_id, tail_id, 1, 255)
+    positive_samples.insert(0,example) 
+    # Add the results to the list
+    all_positive_samples.append(positive_samples)
+
+# Save the combined results to a JSON file
+output_path = "C:/Users/USER/Desktop/json/FB15k237/positive_samples_valid.json"
+with open(output_path, 'w', encoding='utf-8') as f:
+    json.dump(all_positive_samples, f, ensure_ascii=False, indent=4)
+
+end_time = time.time()
+sec = end_time - start_time
+
+print("Total Length:", len(all_positive_samples))
+print("Positive samples of WN18RR saved")
+print("Taking time:",datetime.timedelta(seconds = sec))
+
+print("Done!!")
+"""
+#%%
+path = "C:/Users/USER/Desktop/json/FB15k237/test_HN.json"
+with open(path, 'r', encoding='utf-8') as f:
+    examples = json.load(f)           
+
+dict_total = []
+for index, sublist in enumerate(examples):
+    if len(sublist) != 256:
+        dict_total.append((index, sublist))
+
+print(len(dict_total))
 
